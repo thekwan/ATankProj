32b9a3b72469a269ccf1e9262ba556cc1c04429d
Deokhwan,Kim <thekwan@naver.com>
2021-03-30 21:28:23 +0900

    Keypad frame & key event handler is added.

--- atank/src/atank_frame.cpp
+++ atank/src/atank_frame.cpp
@@ -1,10 +1,17 @@
 #include "atank_frame.h"
 
+#include <map>
+#include <vector>
+#include <algorithm>
+
 const int wxID_ROS_OPEN = 1;
 const int wxID_ROS_CLOSE = 2;
 const int wxID_ROS_TEST = 5;
 const int wxID_UART_OPEN = 3;
 const int wxID_UART_CLOSE = 4;
+const int wxID_KEYCTRL_PAD = 6;
+const int wxID_CLEAR_LOG = 7;
+
 
 ATankFrame::ATankFrame(const wxString & title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxDefaultSize),
@@ -13,21 +20,61 @@ ATankFrame::ATankFrame(const wxString & title)
     /*
      * Menubar and Menu.
      */
-    menubar = new wxMenuBar;
-    file = new wxMenu;
-    file->Append(wxID_ROS_OPEN, wxT("ROS &Open"));
-    file->Append(wxID_ROS_CLOSE, wxT("ROS &Close"));
-    file->Append(wxID_ROS_TEST, wxT("ROS &Test"));
-    file->AppendSeparator();
+    menuFile = new wxMenu;
+    menuFile->Append(wxID_ROS_OPEN,  "ROS &Open",
+            "Initializes ROS client program.");
+    menuFile->Append(wxID_ROS_CLOSE, "ROS &Close",
+            "Deinitializes ROS client program.");
+    menuFile->Append(wxID_ROS_TEST,  "ROS &Test",
+            "Test ROS connection.");
+    menuFile->AppendSeparator();
 
-    file->Append(wxID_UART_OPEN, wxT("UART &Connect"));
-    file->Append(wxID_UART_OPEN, wxT("UART &Disconnect"));
-    file->AppendSeparator();
+    menuFile->Append(wxID_CLEAR_LOG, "Clear &Log",
+            "Clear log window.");
+    menuFile->AppendSeparator();
 
-    file->Append(wxID_EXIT, wxT("&Quit\tCtrl+Q"));
+    menuFile->Append(wxID_KEYCTRL_PAD, "&Key Pad\tK", 
+            "Pop-up key pad pannel to control tank.");
+    menuFile->AppendSeparator();
 
-    menubar->Append(file, wxT("&File"));
+    menuFile->Append(wxID_EXIT, wxT("&Quit\tCtrl+Q"));
 
+
+    // the "Help" menu list
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT, "&About\tF1", "Show about dialog");
+
+    // now append the freshly created menu to the menu bar...
+    menuBar = new wxMenuBar();
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+
+    SetMenuBar(menuBar);
+
+
+    // Log window
+    m_logText = new wxTextCtrl(this, wxID_ANY, "",
+                               wxDefaultPosition, wxDefaultSize,
+                               wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH|wxHSCROLL);
+
+    // set monospace font to have output in nice columns
+    wxFont font(10, wxFONTFAMILY_TELETYPE,
+                wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+    m_logText->SetFont(font);
+
+
+    // layout
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    //sizer->Add(m_inputWin, wxSizerFlags().Expand());
+    sizer->Add(m_logText, wxSizerFlags(1).Expand());
+    SetSizerAndFit(sizer);
+
+    // set size and position on screen
+    SetSize(700, 340);
+    CentreOnScreen();
+
+
+    // connect menu event and handlers
     Connect(wxID_ROS_OPEN, wxEVT_COMMAND_MENU_SELECTED,
             wxCommandEventHandler(ATankFrame::OnRosOpen));
     Connect(wxID_ROS_CLOSE, wxEVT_COMMAND_MENU_SELECTED,
@@ -35,25 +82,42 @@ ATankFrame::ATankFrame(const wxString & title)
     Connect(wxID_ROS_TEST, wxEVT_COMMAND_MENU_SELECTED,
             wxCommandEventHandler(ATankFrame::OnRosTest));
 
+    Connect(wxID_ABOUT, wxEVT_COMMAND_MENU_SELECTED,
+            wxCommandEventHandler(ATankFrame::OnAbout));
+    Connect(wxID_CLEAR_LOG, wxEVT_COMMAND_MENU_SELECTED,
+            wxCommandEventHandler(ATankFrame::OnClear));
+    // Key-Event handler connection.
+    Connect(wxID_KEYCTRL_PAD, wxEVT_COMMAND_MENU_SELECTED,
+            wxCommandEventHandler(ATankFrame::OnKeyControlPad));
+
+
     Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED,
             wxCommandEventHandler(ATankFrame::OnQuit));
 
-    SetMenuBar(menubar);
+    // status bar is useful for showing the menu items help strings
+    CreateStatusBar();
+
 
     /* ROS initialize.
      */
     RosInit();
 
-    Centre();
+    Show(true);
 }
 
 ATankFrame::~ATankFrame()
 {
+    if (m_keypad_frame) {
+        delete m_keypad_frame;
+    }
     RosShutdown();
 }
 
 void ATankFrame::OnQuit(wxCommandEvent & WXUNUSED(event))
 {
+    if (m_keypad_frame) {
+        delete m_keypad_frame;
+    }
     Close(true);
 }
 
@@ -92,6 +156,7 @@ void ATankFrame::OnRosOpen(wxCommandEvent & WXUNUSED(event))
         *_client = _nh->serviceClient<atank::Command>(topic_name.c_str());
 
         ROS_INFO("[CMD client] Service instance is initialized.");
+        m_logText->AppendText("Success to open ROS connection.\n");
         _ros_connected = true;
     }
 }
@@ -120,3 +185,312 @@ void ATankFrame::OnRosTest(wxCommandEvent & WXUNUSED(event))
         ROS_INFO("[CMD CLIENT] there is no connection.");
     }
 }
+
+
+// event handlers
+void ATankFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox("Atank contoller GUI in wxWidgets\n"
+                 "(c) 2020 Deokhwan Kim\n"
+                 "(c) 2020 proto19 tech",
+                 "About Atank controller",
+                 wxOK | wxICON_INFORMATION, this);
+}
+
+void ATankFrame::OnKeyControlPad(wxCommandEvent& WXUNUSED(event))
+{
+    if (m_keypad_frame == nullptr) {
+        m_keypad_frame = new KeyPadFrame("Control pannel", m_logText, this);
+    }
+}
+
+// helper function that returns textual description of wx virtual keycode
+const char* GetVirtualKeyCodeName(int keycode)
+{
+    switch ( keycode )
+    {
+#define WXK_(x) \
+        case WXK_##x: return #x;
+
+        WXK_(BACK)
+        WXK_(TAB)
+        WXK_(RETURN)
+        WXK_(ESCAPE)
+        WXK_(SPACE)
+        WXK_(DELETE)
+        WXK_(START)
+        WXK_(LBUTTON)
+        WXK_(RBUTTON)
+        WXK_(CANCEL)
+        WXK_(MBUTTON)
+        WXK_(CLEAR)
+        WXK_(SHIFT)
+        WXK_(ALT)
+        WXK_(CONTROL)
+        WXK_(MENU)
+        WXK_(PAUSE)
+        WXK_(CAPITAL)
+        WXK_(END)
+        WXK_(HOME)
+        WXK_(LEFT)
+        WXK_(UP)
+        WXK_(RIGHT)
+        WXK_(DOWN)
+        WXK_(SELECT)
+        WXK_(PRINT)
+        WXK_(EXECUTE)
+        WXK_(SNAPSHOT)
+        WXK_(INSERT)
+        WXK_(HELP)
+        WXK_(NUMPAD0)
+        WXK_(NUMPAD1)
+        WXK_(NUMPAD2)
+        WXK_(NUMPAD3)
+        WXK_(NUMPAD4)
+        WXK_(NUMPAD5)
+        WXK_(NUMPAD6)
+        WXK_(NUMPAD7)
+        WXK_(NUMPAD8)
+        WXK_(NUMPAD9)
+        WXK_(MULTIPLY)
+        WXK_(ADD)
+        WXK_(SEPARATOR)
+        WXK_(SUBTRACT)
+        WXK_(DECIMAL)
+        WXK_(DIVIDE)
+        WXK_(F1)
+        WXK_(F2)
+        WXK_(F3)
+        WXK_(F4)
+        WXK_(F5)
+        WXK_(F6)
+        WXK_(F7)
+        WXK_(F8)
+        WXK_(F9)
+        WXK_(F10)
+        WXK_(F11)
+        WXK_(F12)
+        WXK_(F13)
+        WXK_(F14)
+        WXK_(F15)
+        WXK_(F16)
+        WXK_(F17)
+        WXK_(F18)
+        WXK_(F19)
+        WXK_(F20)
+        WXK_(F21)
+        WXK_(F22)
+        WXK_(F23)
+        WXK_(F24)
+        WXK_(NUMLOCK)
+        WXK_(SCROLL)
+        WXK_(PAGEUP)
+        WXK_(PAGEDOWN)
+        WXK_(NUMPAD_SPACE)
+        WXK_(NUMPAD_TAB)
+        WXK_(NUMPAD_ENTER)
+        WXK_(NUMPAD_F1)
+        WXK_(NUMPAD_F2)
+        WXK_(NUMPAD_F3)
+        WXK_(NUMPAD_F4)
+        WXK_(NUMPAD_HOME)
+        WXK_(NUMPAD_LEFT)
+        WXK_(NUMPAD_UP)
+        WXK_(NUMPAD_RIGHT)
+        WXK_(NUMPAD_DOWN)
+        WXK_(NUMPAD_PAGEUP)
+        WXK_(NUMPAD_PAGEDOWN)
+        WXK_(NUMPAD_END)
+        WXK_(NUMPAD_BEGIN)
+        WXK_(NUMPAD_INSERT)
+        WXK_(NUMPAD_DELETE)
+        WXK_(NUMPAD_EQUAL)
+        WXK_(NUMPAD_MULTIPLY)
+        WXK_(NUMPAD_ADD)
+        WXK_(NUMPAD_SEPARATOR)
+        WXK_(NUMPAD_SUBTRACT)
+        WXK_(NUMPAD_DECIMAL)
+        WXK_(NUMPAD_DIVIDE)
+
+        WXK_(WINDOWS_LEFT)
+        WXK_(WINDOWS_RIGHT)
+#ifdef __WXOSX__
+        WXK_(RAW_CONTROL)
+#endif
+#undef WXK_
+
+    default:
+        return NULL;
+    }
+}
+
+// helper function that returns textual description of key in the event
+wxString GetKeyName(const wxKeyEvent &event)
+{
+    int keycode = event.GetKeyCode();
+    const char* virt = GetVirtualKeyCodeName(keycode);
+    if ( virt )
+        return virt;
+    if ( keycode > 0 && keycode < 32 )
+        return wxString::Format("Ctrl-%c", (unsigned char)('A' + keycode - 1));
+    if ( keycode >= 32 && keycode < 128 )
+        return wxString::Format("'%c'", (unsigned char)keycode);
+
+#if wxUSE_UNICODE
+    int uc = event.GetUnicodeKey();
+    if ( uc != WXK_NONE )
+        return wxString::Format("'%c'", uc);
+#endif
+
+    return "unknown";
+}
+
+
+// Key-pad frame constructor
+KeyPadFrame::KeyPadFrame(const wxString& title, wxTextCtrl *p_logText, ATankFrame *parent)
+       : wxFrame(NULL, wxID_ANY, title),
+         m_inputWin(NULL), m_parent(parent)
+{
+
+    m_logText = p_logText;
+    m_logText->AppendText("Keyboard control pannel is opened.\n");
+
+
+    m_inputWin = new wxWindow(this, wxID_ANY,
+                              wxDefaultPosition, wxSize(-1, 50),
+                              wxRAISED_BORDER);
+
+    m_inputWin->SetBackgroundColour(*wxBLUE);
+    m_inputWin->SetForegroundColour(*wxWHITE);
+
+
+    // connect event handlers for the blue input window
+    m_inputWin->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(KeyPadFrame::OnKeyDown),
+                        NULL, this);
+    m_inputWin->Connect(wxEVT_KEY_UP, wxKeyEventHandler(KeyPadFrame::OnKeyUp),
+                        NULL, this);
+
+    m_inputWin->Connect(wxEVT_PAINT,
+                        wxPaintEventHandler(KeyPadFrame::OnPaintInputWin),
+                        NULL, this);
+
+    this->Connect(wxEVT_CLOSE_WINDOW,
+                        wxCloseEventHandler(KeyPadFrame::OnCloseWindow),
+                        NULL, this);
+
+    // layout
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(m_inputWin, wxSizerFlags(1).Expand());
+    SetSizerAndFit(sizer);
+
+    // set size and position on screen
+    SetSize(300, 200);
+    //CentreOnScreen();
+    
+
+    // Add valid keycodes.
+    _valid_keycode.push_back(314);  // left-arrow.
+    _valid_keycode.push_back(315);  // right-arrow.
+    _valid_keycode.push_back(316);  // up-arrow.
+    _valid_keycode.push_back(317);  // down-arrow.
+
+    Show(true);
+}
+
+void KeyPadFrame::OnPaintInputWin(wxPaintEvent& WXUNUSED(event))
+{
+    wxPaintDC dc(m_inputWin);
+    dc.SetTextForeground(*wxWHITE);
+    wxFont font(*wxSWISS_FONT);
+    font.SetWeight(wxFONTWEIGHT_BOLD);
+    font.SetPointSize(font.GetPointSize() + 2);
+    dc.SetFont(font);
+
+    dc.DrawLabel("Press keys here",
+                 m_inputWin->GetClientRect(), wxALIGN_CENTER);
+}
+
+void KeyPadFrame::OnCloseWindow(wxCloseEvent & event)
+{
+    m_logText->AppendText("Keyboard control pannel is closed.\n");
+    m_parent->keypad_frame_closed();
+
+    event.Skip();
+}
+
+void KeyPadFrame::LogEvent(const wxString& name, wxKeyEvent& event)
+{
+    wxString msg;
+#if 1
+    msg.Printf("[%7s] %15s %5d\n", name, GetKeyName(event), event.GetKeyCode());
+#else
+    // event  key_name  KeyCode  modifiers  Unicode  raw_code raw_flags pos
+    msg.Printf("%7s %15s %5d   %c%c%c%c"
+#if wxUSE_UNICODE
+                   "%5d (U+%04x)"
+#else
+                   "    none   "
+#endif
+#ifdef wxHAS_RAW_KEY_CODES
+                   "  %7lu    0x%08lx"
+#else
+                   "  not-set    not-set"
+#endif
+                   "  (%5d,%5d)"
+                   "\n",
+               name,
+               GetKeyName(event),
+               event.GetKeyCode(),
+               event.ControlDown() ? 'C' : '-',
+               event.AltDown()     ? 'A' : '-',
+               event.ShiftDown()   ? 'S' : '-',
+               event.MetaDown()    ? 'M' : '-'
+#if wxUSE_UNICODE
+               , event.GetUnicodeKey()
+               , event.GetUnicodeKey()
+#endif
+#ifdef wxHAS_RAW_KEY_CODES
+               , (unsigned long) event.GetRawKeyCode()
+               , (unsigned long) event.GetRawKeyFlags()
+#endif
+               , event.GetX()
+               , event.GetY()
+               );
+#endif
+    m_logText->AppendText(msg);
+}
+
+bool KeyPadFrame::isInvalidKeyCode(int keycode)
+{
+    auto it = std::find(_valid_keycode.begin(), _valid_keycode.end(), keycode);
+    return (it == _valid_keycode.end());
+}
+
+void KeyPadFrame::OnKeyDown(wxKeyEvent& event) {
+    int keycode = event.GetKeyCode();
+
+    // check the valid key event.
+    if (isInvalidKeyCode(keycode))
+        return;
+    
+    if (key_state.find(keycode) == key_state.end()
+     || key_state[keycode] == KeyState::KEY_RELEASED) {
+        LogEvent("KeyDown", event);
+        key_state[keycode] = KeyState::KEY_PUSHED;
+    }
+}
+
+void KeyPadFrame::OnKeyUp(wxKeyEvent& event) {
+    int keycode = event.GetKeyCode();
+
+    // check the valid key event.
+    if (isInvalidKeyCode(keycode))
+        return;
+
+    if (key_state.find(keycode) == key_state.end()
+     || key_state[keycode] == KeyState::KEY_PUSHED) {
+        LogEvent("KeyUp", event);
+        key_state[keycode] = KeyState::KEY_RELEASED;
+    }
+}
+
--- atank/src/atank_frame.h
+++ atank/src/atank_frame.h
@@ -4,11 +4,19 @@
 #include "ros/ros.h"
 #include "atank/Command.h"
 
+class KeyPadFrame;
 class ATankFrame : public wxFrame
 {
 public:
     ATankFrame(const wxString & title);
     ~ATankFrame();
+
+    /*
+     * Keypad frame member funcs.
+     */
+    void keypad_frame_closed(void) {
+        m_keypad_frame = nullptr;
+    }
 
 private:
     /* 
@@ -23,11 +31,48 @@ private:
     void OnRosClose(wxCommandEvent & WXUNUSED(event));
     void OnRosTest(wxCommandEvent & WXUNUSED(event));
 
+    void OnAbout(wxCommandEvent& event);
+    void OnClear(wxCommandEvent& WXUNUSED(event)) { m_logText->Clear(); }
+    void OnKeyControlPad(wxCommandEvent& WXUNUSED(event));
+
     /*
      * GUI member variables and functions
      */
-    wxMenuBar *menubar;
-    wxMenu    *file;
-    //wxMenu    *tools;
+    wxMenuBar *menuBar;
+    wxMenu    *menuFile;
+    wxMenu    *menuHelp;
+    
+    wxTextCtrl *m_logText;          // log window
+    KeyPadFrame *m_keypad_frame;    // key frame handler
+
     void OnQuit(wxCommandEvent & WXUNUSED(event));
+};
+
+class KeyPadFrame : public wxFrame
+{
+public:
+    KeyPadFrame(const wxString& title, wxTextCtrl *p_logText, ATankFrame *m_parent);
+
+private:
+    void OnKeyDown(wxKeyEvent& event);
+    void OnKeyUp(wxKeyEvent& event);
+
+    void OnCloseWindow(wxCloseEvent & event);
+
+    void LogEvent(const wxString& name, wxKeyEvent& event);
+    bool isInvalidKeyCode(int keycode);
+
+    // event handlers
+    void OnPaintInputWin(wxPaintEvent& event);
+
+    wxWindow   *m_inputWin;
+    ATankFrame *m_parent;
+    wxTextCtrl *m_logText;
+
+    enum class KeyState{
+        KEY_PUSHED,
+        KEY_RELEASED
+    };
+    std::map<int, KeyState> key_state;
+    std::vector<int> _valid_keycode;
 };
