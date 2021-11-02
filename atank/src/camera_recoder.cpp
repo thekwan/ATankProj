7b695989eeb398b20c81d04f3d239aceb57c37d3
Deokhwan,Kim <thekwan@naver.com>
2021-11-02 21:44:59 +0900

    (camera_recoder) add execute command queue.

--- atank/src/camera_recoder.cpp
+++ atank/src/camera_recoder.cpp
@@ -2,7 +2,9 @@
 #include "std_msgs/String.h"
 #include "atank/Command.h"
 
+#include <chrono>
 #include <thread>
+#include <queue>
 #include <signal.h>
 
 void mySignalHandler(int sig) {
@@ -10,6 +12,66 @@ void mySignalHandler(int sig) {
     ros::shutdown();
     exit(1);
 }
+
+#define TIME_TO_SLEEP   1000
+
+class CommandProcessor {
+public:
+    CommandProcessor(ros::NodeHandle &node) {
+        node_ = node;
+        client_ = node_.serviceClient<atank::Command>("command");
+    }
+    ~CommandProcessor(void) {
+    }
+    int size(void) {
+        return cmd_queue_.size();
+    }
+    void pushCommand(const char *command) {
+        cmd_queue_.push(std::string(command));
+    }
+    void executeCommand(void) {
+        std::string cmd = cmd_queue_.front();
+        cmd_queue_.pop();
+
+        if (cmd.compare("wait_10ms") == 0) {
+            std::cout << "[DEBUG] wait 10 msec" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(TIME_TO_SLEEP));
+        }
+        else if (cmd.compare("left_turn") == 0) {
+            requestRosService("left.motor.fw");
+        }
+        else if (cmd.compare("right_turn") == 0) {
+            requestRosService("right.motor.fw");
+        }
+        else if (cmd.compare("version") == 0) {
+            requestRosService("version");
+        }
+        else {
+            std::cerr << "[ERROR] unknown instruction!" << std::endl;
+        }
+    }
+private:
+    void requestRosService(const char *cmd) {
+        atank::Command tankCmd;
+        tankCmd.request.cmd = std::string(cmd);
+
+        if (client_.call(tankCmd)) {
+            if (tankCmd.response.ack == true) {
+                ROS_INFO("[CameraRecoder] request service: %s", cmd);
+                ROS_INFO("[CameraRecoder] ack: '%s'", tankCmd.response.log.c_str());
+            }
+            else {
+                ROS_INFO("[CameraRecoder] connection is failed.");
+            }
+        }
+        else {
+            ROS_INFO("[CameraRecoder] failed to call service.");
+        }
+    }
+    ros::NodeHandle node_;
+    ros::ServiceClient client_;
+    std::queue<std::string> cmd_queue_;
+};
 
 int main(int argc, char *argv[])
 {
@@ -21,22 +83,15 @@ int main(int argc, char *argv[])
     signal(SIGINT, mySignalHandler);
 
     // "command": topic name
-    ros::ServiceClient client = n.serviceClient<atank::Command>("command");
+    CommandProcessor cproc(n);
 
-    atank::Command tankCmd;
-    tankCmd.request.cmd = std::string("version");
+    cproc.pushCommand("wait_10ms");
+    cproc.pushCommand("version");
+    cproc.pushCommand("wait_10ms");
+    cproc.pushCommand("wait_10ms");
 
-    if (client.call(tankCmd)) {
-        if (tankCmd.response.ack == true) {
-            ROS_INFO("[CameraRecoder] connection is successful.");
-            ROS_INFO("[CameraRecoder] ack: %s", tankCmd.response.log.c_str());
-        }
-        else {
-            ROS_INFO("[CameraRecoder] connection is failed.");
-        }
-    }
-    else {
-        ROS_INFO("[CameraRecoder] failed to call service.");
+    while(cproc.size() > 0) {
+        cproc.executeCommand();
     }
 
     return 0;
